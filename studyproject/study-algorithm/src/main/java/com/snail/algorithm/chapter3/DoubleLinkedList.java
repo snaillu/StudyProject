76816634c4e4076eab767232e666606b90182c9f
snail <luqu@live.cn>
2015-04-23 19:22:37 +0800

    [Add] add chapter5

---
+++ studyproject/study-algorithm/src/main/java/com/snail/algorithm/chapter3/DoubleLinkedList.java
@@ -0,0 +1,69 @@
+package com.snail.algorithm.chapter3;
+
+public class DoubleLinkedList<E> {
+	private static class Node<E>{
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+		public Node(E e,Node<E> p,Node<E> n){
+			element = e;
+			prev = p;
+			next = n;
+		}
+		public E getElement(){return element;}
+		public Node<E> getPrev(){return prev;}
+		public Node<E> getNext(){return next;}
+		public void setPrev(Node<E> p){prev = p;}
+		public void setNext(Node<E> n){next = n;}
+	}
+	
+	//instance variables of the DoublyLinkedList
+	private Node<E> header;
+	private Node<E> tailer;
+	private int size = 0;
+	//constructs
+	public DoubleLinkedList(){
+		header = new Node<E>(null,null,null);
+		tailer = new Node<E>(null,header,null);
+		header.setNext(tailer);
+	}
+	
+	public int size(){return size;}
+	public boolean isEmpty(){return size==0;}
+	public E first(){
+		if(isEmpty()) return null;
+		return header.getNext().getElement();
+	}
+	public E last(){
+		if(isEmpty()) return null;
+		return tailer.getPrev().getElement();
+	}
+	public void addFirst(E e){
+		addBetween(e,header,header.getNext());
+	}
+	public void addLast(E e){
+		addBetween(e,tailer.getPrev(),tailer);
+	}
+	public E removeFirst(){
+		if(isEmpty()) return null;
+		return remove(header.getNext());
+	}
+	public E removeLast(){
+		if(isEmpty()) return null;
+		return remove(tailer.getPrev());
+	}
+	private void addBetween(E e,Node<E> predecessor,Node<E> successor){
+		Node<E> newest = new Node<E>(e,predecessor,successor);
+		predecessor.setNext(newest);
+		successor.setPrev(newest);
+		size++;
+	}
+	private E remove(Node<E> node){
+		Node<E> predesscessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predesscessor.setNext(successor);
+		successor.setPrev(predesscessor);
+		size--;
+		return node.getElement();
+	}
+}
---
+++ studyproject/study-algorithm/src/main/java/com/snail/algorithm/chapter5/Algorithms.java
@@ -0,0 +1,85 @@
+package com.snail.algorithm.chapter5;
+
+import java.io.File;
+
+public class Algorithms {
+	//factorial
+	public static int factorial(int n) throws IllegalArgumentException{
+		if(n<0)
+			throw new IllegalArgumentException();
+		else if(n==0)
+			return 1;
+		else
+			return n*factorial(n-1);
+	}
+	
+	//draw English ruler
+	public static void drawRuler(int nInches,int majorLength){
+		drawLine(majorLength,0);
+		for(int j=1;j<=nInches;j++){
+			drawInterval(majorLength-1);
+			drawLine(majorLength,j);
+		}
+	}
+	
+	private static void drawInterval(int centralLength){
+		if(centralLength>=1){
+			drawInterval(centralLength-1);
+			drawLine(centralLength);
+			drawInterval(centralLength-1);
+		}
+	}
+	
+	private static void drawLine(int tickLength,int tickLabel){
+		for(int j=0;j<tickLength;j++)
+			System.out.print("-");
+		if(tickLabel>=0)
+			System.out.print(" "+tickLabel);
+		System.out.print("\n");
+	}
+	
+	private static void drawLine(int tickLength){
+		drawLine(tickLength,-1);
+	}
+	
+	//binary search
+	public static boolean binarySearch(int[] data,int target,int low,int high){
+		if(low>high)
+			return false;
+		else{
+			int mid = (low+high)/2;
+			if(target==data[mid])
+				return true;
+			else if(target<data[mid])
+				return binarySearch(data,target,low,mid-1);
+			else
+				return binarySearch(data,target,mid+1,high);
+		}
+	}
+	
+	public static long diskUsage(File root){
+		long total = root.length();
+		if(root.isDirectory()){
+			for(String childname:root.list()){
+				File child = new File(root,childname);
+				total += diskUsage(child);
+			}
+		}
+		System.out.println(total+"\t"+root);
+		return total;
+	}
+	
+	public static void main(String[] args) {
+//		int result = factorial(5);
+//		System.out.println("result="+result);
+		
+//		drawRuler(5,2);
+		
+//		int[] data = {1,2,3,4,5,6,7,8,11,19,54,56,78};
+//		boolean result = binarySearch(data,19,0,data.length);
+//		System.out.println("result="+result);
+		
+		File file = new File("D:\\Code");
+		diskUsage(file);
+	}
+}
