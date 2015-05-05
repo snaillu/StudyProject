26777040157c14f3472d2980791a493c52536f1d
snail <luqu@live.cn>
2015-05-05 09:07:06 +0800

    [Mod] del

--- studyproject/study-algorithm/src/main/java/com/snail/algorithm/chapter7/Position.java
+++
@@ -1,5 +0,0 @@
-package com.snail.algorithm.chapter7;
-
-public interface Position<E> {
-	E getElement() throws IllegalStateException;
-}
--- studyproject/study-algorithm/src/main/java/com/snail/algorithm/chapter8/BinaryTree.java
+++
@@ -1,9 +0,0 @@
-package com.snail.algorithm.chapter8;
-
-import com.snail.algorithm.chapter7.Position;
-
-public interface BinaryTree<E> extends Tree<E> {
-	Position<E> left(Position<E> p) throws IllegalArgumentException;
-	Position<E> right(Position<E> p) throws IllegalArgumentException;
-	Position<E> sibling(Position<E> p) throws IllegalArgumentException;
-}
